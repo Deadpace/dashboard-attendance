f73177d583cd951ca835e1da6fe96226ae9f1a22
Rishabh Kalra <rishabhkalra1501@gmail.com>
2021-10-29 14:15:18 +0530

    login-system-added

--- app.py
+++ app.py
@@ -1,54 +1,157 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 import json
+import bcrypt
 import pymongo
+from datetime import timedelta
 
 app = Flask(__name__)
 app.secret_key = "secret!"
 
+#for setting the session time
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
+
+#for reloading the templates
+app.jinja_env.auto_reload = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 #loading config.json file for configurations
 with open('config.json') as f:
     config = json.load(f)
 
+#mongodb connection
 mongodb_url = config["mongodb_url"]
-database_name = config["database_name"]
-
-
 client = pymongo.MongoClient(mongodb_url)
-DBlist = client.list_database_names()
-if database_name in DBlist:
-    print(f"DB: '{database_name}' exists")
-else:
-    print(f"DB: '{database_name}' not yet present OR no collection is present in the DB")
 
-database = client[database_name]
-login_database_name = client[config["login_database_name"]]
-login_collection = login_database_name[config["login_collection_name"]]
+#attendance database
+attendance_database = client[config["attendance_database_name"]]
+
+#login database
+login_collection_name = config[
+    "login_collection_name"]  #collection name from config
+login_database = client[config["login_database_name"]]
+login_collection = login_database[login_collection_name]
+
 
 @app.route('/')
 def index():
-    data = sorted(database.list_collection_names(),reverse=True)
-    dates = [file.split('_')[1] for file in data]
-    return render_template('index.html',dates = enumerate(zip(dates,data)))
+    global login_collection
+    login_collection = login_database[login_collection_name]
+    if 'username' in session:
+        return redirect(url_for('dashboard'))
 
-@app.route('/employee/<string:file_name>')
-def employee(file_name):
-    collection = database[file_name]
-    file = [i for i in collection.find()]
-    return render_template('employee.html',file= file)
+    return render_template('login/login.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.method == "POST":
+        if request.form:
+            login_user = login_collection.find_one({'Email': request.form['email']})
+            if login_user:
+                if login_user["Role"] == 'Admin':
+                    if bcrypt.checkpw(request.form['password'].encode('utf8'),
+                                    login_user['Password']):
+                        session['username'] = login_user['Name']
+                        session['email'] = login_user['Email']
+                        session.permanent = True
+                        return redirect(url_for('index'))
+                else:
+                    return "Access Not Available!!"
+
+            return 'Invalid username/password combination'
+    else:
+        return "Invalid Request"
+
+@app.route('/dashboard')
+def dashboard():
+    if 'username' in session:
+        data = sorted(attendance_database.list_collection_names(),
+                      reverse=True)
+        dates = [file.split('_')[1] for file in data]
+        return render_template('dashboard/index.html',
+                               dates=enumerate(zip(dates, data)))
+    else:
+        return redirect(url_for('index'))
+
+
+@app.route('/employee/<string:collection_name>')
+def employee(collection_name):
+    if 'username' in session:
+        collection = attendance_database[collection_name]
+        content = [i for i in collection.find()]
+        return render_template('dashboard/employee.html', content=content)
+    else:
+        return redirect(url_for('index'))
+
 
 @app.route('/add_employee')
 def add_employee():
-    return render_template("add-employee.html", style = "display: none")
+    if 'username' in session:
+        return render_template("dashboard/add-employee.html")
+    else:
+        return redirect(url_for('index'))
 
-@app.route('/add_into_data',methods=["POST"])
+
+@app.route('/add_into_data', methods=["POST"])
 def add_into_data():
     if request.method == "POST":
         if request.form:
             data = dict(request.form)
-            print(data)
-            login_collection.insert_one(data)
-            return render_template("add-employee.html", style = "")
+            global login_collection
+            existing_user = login_collection.find_one(
+                {'Email': request.form['Email']})
 
-if __name__== "__main__":
-    app.run(debug=True)
+            if not existing_user:
+                data['Password'] = bcrypt.hashpw(
+                    data['Password'].encode('utf-8'), bcrypt.gensalt())
+                login_collection.insert_one(data)
+                login_collection = login_database[login_collection_name]
+                return render_template("dashboard/add-employee.html",
+                                       employee_add=True)
+
+            return render_template("dashboard/add-employee.html",
+                                   email_present=True)
+
+
+@app.route('/change_password', methods=['POST'])
+def change_password():
+    if request.method == 'POST':
+        if request.form:
+            global login_collection
+            user_details = login_collection.find_one(
+                {'Email': session["email"]})
+            if request.form["new_password"] == request.form["confirm_new_password"]:
+                if bcrypt.checkpw(request.form['current_password'].encode('utf8'),
+                        user_details['Password']):
+                    new_password = bcrypt.hashpw(
+                        request.form['new_password'].encode('utf-8'),
+                        bcrypt.gensalt())
+                    login_collection.update_one(
+                        user_details, {"$set": {
+                            "Password": new_password
+                        }})
+                    return redirect(url_for('logout'))
+                else:
+                    return "Current password does not match!!"
+                
+                
+            else:
+                return "New Password and Confirm New Password does not match"
+
+    return redirect(url_for('dashboard'))
+
+
+@app.route('/logout')
+def logout():
+    if 'username' in session:
+        try:
+            session.clear()
+            return redirect(url_for('index'))
+        except KeyError:
+            return redirect(url_for('index'))
+    else:
+        return redirect(url_for('index'))
+
+
+if __name__ == "__main__":
+    app.run(debug=True, threaded=True)
